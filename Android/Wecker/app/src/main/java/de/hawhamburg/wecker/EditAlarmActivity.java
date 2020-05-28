a9ea839c86fbef13b513404613edb20be583bb2d
sin-chen <Sina.Oezugurlu@haw-hamburg.de>
2020-05-28 14:16:40 +0200

    Android Einführung

---
+++ Android/Wecker/app/src/main/java/de/hawhamburg/wecker/EditAlarmActivity.java
@@ -0,0 +1,14 @@
+package de.hawhamburg.wecker;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class EditAlarmActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_alarm);
+    }
+}
